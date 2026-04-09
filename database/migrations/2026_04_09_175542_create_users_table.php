9794c7ca7ae74f9f5c62713bd28a2919ac55d355
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-04-09 18:57:26 +0100

    feat(migrations): added users migration

---
+++ database/migrations/2026_04_09_175542_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('username', 100)->unique();
+            $table->string('display_name', 100)->nullable();
+            $table->foreignId('tenant_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_banned')->default(false);
+            $table->string('banned_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
