fc2c3b1632ad3305b36cd58daba617233472cc2e
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-04-10 17:11:30 +0100

    feat(migrations): added messages migration

---
+++ database/migrations/2026_04_10_160717_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chatable_id');
+            $table->string('chatable_type', 20);
+            $table->index(['chatable_id', 'chatable_type']);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->text('content')->nullable();
+            $table->boolean('is_file')->default(false);
+            $table->string('file_path', 500)->nullable();
+            $table->foreignId('parent_id')->nullable()->constrained('messages')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
