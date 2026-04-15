a15b52033a9d02d5ea5b0e4a872c8ad2dfa89db4
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-04-15 16:25:31 +0100

    feat(models): added tenantRole relation to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,6 +63,11 @@ class User extends Authenticatable
         return $this->belongsTo(Tenant::class);
     }
 
+    public function tenantRole(): BelongsTo
+    {
+        return $this->belongsTo(TenantRole::class);
+    }
+
     public function groups(): BelongsToMany
     {
         return $this->belongsToMany(Group::class, 'group_members')
