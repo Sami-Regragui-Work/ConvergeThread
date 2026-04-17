b1a28a75bd00bde29a37aa56ba9f268d26594b99
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-04-17 16:18:09 +0100

    feat(routes): added invitation API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\InvitationController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -9,4 +10,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
+
+    Route::post('/invitations/owner', [InvitationController::class, 'createOwner']);
+
+    Route::post('/invitations/tenant', [InvitationController::class, 'createTenant']);
 });
+
+Route::get('/invitations/{token}', [InvitationController::class, 'show']);
+Route::post('/invitations/{token}/accept', [InvitationController::class, 'accept']);
