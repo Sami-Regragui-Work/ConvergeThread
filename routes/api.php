2e0d2ae2d6f454e7f8d7bb2d14f1e7a7c298f761
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-04-19 19:28:49 +0100

    feat(routes): added Group API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\GroupController;
 use App\Http\Controllers\Api\InvitationController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,13 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/refresh', [AuthController::class, 'refresh']);
 
     Route::post('/invitations/owner', [InvitationController::class, 'createOwnerInvitation']);
-
     Route::post('/invitations/tenant', [InvitationController::class, 'createTenantInvitation']);
+
+    Route::post('/groups', [GroupController::class, 'store']);
+    Route::get('/groups', [GroupController::class, 'index']);
+    Route::get('/groups/{group}', [GroupController::class, 'show']);
+    Route::patch('/groups/{group}', [GroupController::class, 'update']);
+    Route::delete('/groups/{group}', [GroupController::class, 'destroy']);
 });
 
 Route::get('/invitations/{token}', [InvitationController::class, 'show']);
