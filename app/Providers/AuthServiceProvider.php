83aa0686accaf0f81bd1eebdbbff58f347b395e3
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-05-04 19:55:32 +0100

    feat(provider): registered policy mappings in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,13 +5,25 @@ namespace App\Providers;
 // use Illuminate\Support\ServiceProvider;
 
 use App\Models\Group;
+use App\Models\Invitation;
+use App\Models\MergeSession;
+use App\Models\Message;
+use App\Models\TenantRole;
 use App\Policies\GroupPolicy;
+use App\Policies\InvitationPolicy;
+use App\Policies\MergeSessionPolicy;
+use App\Policies\MessagePolicy;
+use App\Policies\TenantRolePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as VendorAuthServiceProvider;
 
 class AuthServiceProvider extends VendorAuthServiceProvider
 {
     protected $policies = [
         Group::class => GroupPolicy::class,
+        Message::class => MessagePolicy::class,
+        MergeSession::class => MergeSessionPolicy::class,
+        TenantRole::class => TenantRolePolicy::class,
+        Invitation::class => InvitationPolicy::class,
     ];
 
     // /**
