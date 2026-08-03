a77543f72e256dd72d25cd2847f5184c81336e97
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-08-03 13:18:57 +0100

    refactor(user-model): remove stale JWT scaffolding

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,14 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-// use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable /*implements JWTSubject*/
+class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -54,19 +53,6 @@ class User extends Authenticatable /*implements JWTSubject*/
             'updated_at' => 'datetime',
         ];
     }
-
-    // public function getJWTIdentifier()
-    // {
-    //     return $this->getKey();
-    // }
-
-    // public function getJWTCustomClaims()
-    // {
-    //     return [
-    //         'tenant_id' => $this->tenant_id,
-    //         'username' => $this->username,
-    //     ];
-    // }
 
     public function tenant(): BelongsTo
     {
